0c2c231af55ebf603e0c89491b94733a44fc7416
coderaiser <coderaiser@cloudcmd.io>
2026-07-30 05:51:27 +0000

    test: coverage

--- cmd/subscriber/server_test.go
+++ cmd/subscriber/server_test.go
@@ -74,6 +74,16 @@ func TestServeBadPort(t *testing.T) {
 	})
 }
 
+func TestIoDiscardWrite(t *testing.T) {
+	tape.Test(t, "server: ioDiscard.Write returns len and no error", func(t *tape.T) {
+		d := ioDiscard{}
+		n, err := d.Write([]byte("hello"))
+		t.Ok(n == 5)
+		t.Ok(err == nil)
+		t.End()
+	})
+}
+
 func TestServerHealthzIntegration(t *testing.T) {
 	tape.Test(t, "server: integration GET /healthz returns ok", func(t *tape.T) {
 		base, cancel := startTestServer(context.Background())
